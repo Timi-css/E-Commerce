ce20ea2a305796111b7e31398d16cfc619e07ed4
Timi <alabitimi65@gmail.com>
2022-10-07 09:23:37 -0600

    verify token for user authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -28,12 +29,23 @@ router.post("/login", async (req, res) => {
       user.password,
       process.env.PASS_SEC
     );
-    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const MainPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    password !== req.body.password &&
+    MainPassword !== req.body.password &&
       res.status(401).json("Invalid Credentials");
 
-    res.status(200).json(user);
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user._isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,29 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAuthorization } = require("./verifyToken");
 const router = require("express").Router();
+
+router.put("/:id", verifyTokenAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
 
 module.exports = router;
